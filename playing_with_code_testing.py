3791f94059e0e1ee79c72aed3bb39cf9dfef2c5d
NathanHundley <40145427+NathanHundley@users.noreply.github.com>
2019-09-09 18:04:52 -0500

    Add files via upload

---
+++ playing_with_code_testing.py
@@ -0,0 +1,253 @@
+##############################################################
+##Playing with Testing Code
+##############################################################
+
+#Creating a function to test
+
+def get_formatted_name(first, last):
+    """Generate a nearly formatted full name."""
+    full_name = f"{first} {last}"
+    return full_name.title()
+
+#Creating unit test
+
+import unittest
+
+class NameTestCase(unittest.TestCase):
+    """Tests for get_formatted_name"""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+if __name__ == '__main__':
+    unittest.main()
+
+#Failing test example
+
+def get_formatted_name(first, middle, last):
+    """Generate a nearly formatted full name."""
+    full_name = f"{first} {middle} {last}"
+    return full_name.title()
+
+import unittest
+
+class NameTestCase(unittest.TestCase):
+    """Tests for get_formatted_name"""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+if __name__ == '__main__':
+    unittest.main()
+
+#When you are addressing a failed test, edit the function to make it pass,
+#do not change the test
+
+def get_formatted_name(first, last, middle=''):
+    """Generate a neatly formatted full name."""
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
+    return full_name.title()
+
+import unittest
+
+class NameTestCase(unittest.TestCase):
+    """Tests for get_formatted_name"""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+if __name__ == '__main__':
+    unittest.main()
+
+#Adding new tests
+
+def get_formatted_name(first, last, middle=''):
+    """Generate a neatly formatted full name."""
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
+    return full_name.title()
+
+import unittest
+
+class NameTestCase(unittest.TestCase):
+    """Tests for get_formatted_name"""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart work?"""
+        formatted_name = get_formatted_name(
+            'wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+##############################################################
+##Testing Classes
+##############################################################
+
+#Types of assert options:
+    #'assertEqual(a, b)', verifies that a == b
+    #'assertNotEqual(a, b)', verified that a != b
+    #'assertTrue(x)', verifies that x is True
+    #'assertFalse(x)', verifies that x is False
+    #'assertIn(item, list)', verifies that item is in list
+    #'assertNotIn(item, list)', verifies that item is not in list
+
+#A class to test
+
+class AnonymousSurvey:
+    """Collect anonymous answers to a survey question."""
+
+    def __init__(self, question):
+        """Store a question, and prepare to store responses."""
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """Show the survey question."""
+        print(self.question)
+
+    def store_response(self, new_response):
+        """Store a single response to the survey."""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """Show all the responses that have been given."""
+        print("Survey results:")
+        for response in self.responses:
+            print(f"- {response}")
+
+question = "Do you enjoy attending social events?"
+my_survey = AnonymousSurvey(question)
+
+my_survey.show_question()
+print("Enter 'q' at any time to quit.\n")
+
+while True:
+    response = input("Agree or Disagree: ")
+    if response == 'q':
+        break
+    my_survey.store_response(response)
+
+print("\nThank you to everyone who participated in the survey!")
+my_survey.show_results()
+
+#Testing the anonymous class
+
+import unittest
+
+class AnonymousSurvey:
+    """Collect anonymous answers to a survey question."""
+
+    def __init__(self, question):
+        """Store a question, and prepare to store responses."""
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """Show the survey question."""
+        print(self.question)
+
+    def store_response(self, new_response):
+        """Store a single response to the survey."""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """Show all the responses that have been given."""
+        print("Survey results:")
+        for response in self.responses:
+            print(f"- {response}")
+
+class TestAnonymousSurvey(unittest.TestCase):
+    """Tests for the class AnonymousSurvey"""
+
+    def test_store_single_response(self):
+        """Test that a single response is stored properly."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('English')
+        self.assertIn('English', my_survey.responses)
+
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+
+if __name__ == '__main__':
+    unittest.main()
+
+#Using the setUp() method
+
+import unittest
+
+class AnonymousSurvey:
+    """Collect anonymous answers to a survey question."""
+
+    def __init__(self, question):
+        """Store a question, and prepare to store responses."""
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """Show the survey question."""
+        print(self.question)
+
+    def store_response(self, new_response):
+        """Store a single response to the survey."""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """Show all the responses that have been given."""
+        print("Survey results:")
+        for response in self.responses:
+            print(f"- {response}")
+
+class TestAnonymousSurvey(unittest.TestCase):
+    """Tests for the class AnonymousSurvey"""
+
+    def setUp(self):
+        """
+        Create a survey and a set of responses for use in all test methods.
+        """
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
+    def test_store_single_response(self):
+        """Test that a single response is stored properly."""
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
+if __name__ == '__main__':
+    unittest.main()
